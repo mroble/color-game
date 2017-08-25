e3632d48b76864e92a24f56c5ab384ab1e130235
Maureen Roble <moroble@gmail.com>
2017-08-25 16:40:03 -0400

    corrected js error

--- colorGame.js
+++ colorGame.js
@@ -11,5 +11,5 @@ var colors = [
 var squares = document.querySelectorAll(".square");
 
 for(var i = 0; i < squares.length; i++){
-    squares[i.style.backgroundColor = colors[i];
+    squares[i].style.backgroundColor = colors[i];
 }
