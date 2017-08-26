9bf6aaa24d2015888aa17567783e9774f33823be
Maureen Roble <moroble@gmail.com>
2017-08-25 21:54:24 -0400

    add simple display logic

--- colorGame.js
+++ colorGame.js
@@ -11,6 +11,8 @@ var colors = [
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+
 
 colorDisplay.textContent = pickedColor;
 
@@ -24,10 +26,14 @@ for(var i = 0; i < squares.length; i++){
     var clickedColor = this.style.backgroundColor;
     //compare color to pickedColor
     if (clickedColor === pickedColor) {
-        alert("Correct!");
+        messageDisplay.textContent = "Correct!";
     } else {
-        alert("WRONG");
+        this.style.backgroundColor = "#232323";
+        messageDisplay.textContent = "Try Again!";
     }
     });
+}
+
+function changeColors(){
 
 }
